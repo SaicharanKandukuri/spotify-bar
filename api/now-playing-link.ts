74f6204feb9104205e13741011f9e2e162e1a72f
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-12-11 14:38:22 +0530

    redirect to user profile when playing nothing

--- api/now-playing-link.ts
+++ api/now-playing-link.ts
@@ -4,18 +4,16 @@ import { getNowPlaying, getProfile } from "../lib/spotify";
 export default async function nowPlaying(_: VercelRequest, res: VercelResponse) {
     const nowPlaying = await getNowPlaying();
     const profile = await getProfile();
-    
-    const externalUrl = nowPlaying.externalUrl;
-    const profileUrl = profile.profileUrl;
 
-    // if length of @externalUrl is not null, redirect to @externalUrl
-    if (externalUrl === null) {
+    // check if the user is playing music
+    if (nowPlaying === null) {
         // redirect to the external URL
-        console.log(`Redirecting to ${externalUrl}`);
-        return res.redirect(externalUrl)
-    
-    } else {
-       profileUrl.length === 0 ? res.status(404).send("Not found") : res.redirect(profileUrl);
+        console.log(`Redirecting to ${profile.profileUrl}`);
+        return res.redirect(profile.profileUrl)
     }
-
+    else {
+        // redirect to the external URL
+        console.log(`Redirecting to ${nowPlaying.externalUrl}`);
+        return res.redirect(nowPlaying.externalUrl)
+    }
 }
