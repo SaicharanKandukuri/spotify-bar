2d0cf4c5d01e1f15ecf468f7f168677ab9314632
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-12-11 11:33:31 +0530

    fix deprecated method (or whatever they are)

--- api/now-playing-link.ts
+++ api/now-playing-link.ts
@@ -1,7 +1,7 @@
-import { NowRequest, NowResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getNowPlaying } from "../lib/spotify";
 
-export default async function nowPlaying(_: NowRequest, res: NowResponse) {
+export default async function nowPlaying(_: VercelRequest, res: VercelResponse) {
     const nowPlaying = await getNowPlaying();
     const externalUrl = nowPlaying.externalUrl;
 
